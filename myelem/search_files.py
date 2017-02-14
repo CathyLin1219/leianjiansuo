af0396729e9e8af4a6eba52fbf983e2ebcc433a2
gwlin <gwlin@iflytek.com>
2017-02-14 10:35:18 +0800

    要素的评价方法进行细化，主要添加“杀人”与“盗窃”的要素及其得分值。按照罪名检查并计算检索文书及候选文书的各要素得分，用欧氏距离作为相似度打分

--- myelem/search_files.py
+++ myelem/search_files.py
@@ -1,14 +1,97 @@
 #!/usr/bin/env python
 # coding=UTF-8
-import sys, subprocess, os, re
+import re
 import numpy as np
 import patterns
-from common import path_define, defines
-
+from common import path_define, defines, utils
+import numpy as np
 
 class elem_analysis:
-    def __init__(self):
-        pass
+    def __init__(self, crime_index):
+        self.elemdict = dict(patterns.sp_pattern_dict[defines.crime_list[crime_index]], **patterns.com_pattern_dict)
+        self.numelemdict = patterns.sp_num_pattern_dict[defines.crime_list[crime_index]]
+
+    def analysis_sp_elem(self, text):
+        sp_elem_dict = {}
+        # 记录数值相关要素的值
+        for item in self.numelemdict.keys():
+            pt_obj = re.compile(patterns.pattern_dict[item][0])
+            if item == 'money':
+                lst = pt_obj.findall(text)
+                if len(lst) > 0:
+                    # 取最后一个匹配的结果
+                    sp_elem_dict[item] = utils.chn2int(lst[len(lst) - 1])
+                else:
+                    sp_elem_dict[item] = 0
+            elif item == 'count' or item == 'death_count':
+                lst = pt_obj.findall(text)
+                sp_elem_dict[item] = 0
+                if len(lst) > 0:
+                    sp_elem_dict[item] = 1
+                for it in lst:
+                    if it != '':
+                        sp_elem_dict[item] = utils.chn2int(it)
+            elif item == 'injure_count':
+                lst = pt_obj.findall(text)
+                qingshang = 0
+                zhongshang = 0
+                for tup in lst:
+                    if tup[1] == u'轻伤':
+                        if utils.chn2int(tup[0]) == 0:
+                            qingshang = 1
+                        else:
+                            qingshang = utils.chn2int(tup[0])
+                    else:   # 重伤
+                        if utils.chn2int(tup[0]) == 0:
+                            zhongshang = 1
+                        else:
+                            zhongshang = utils.chn2int(tup[0])
+                sp_elem_dict[item] = (qingshang, zhongshang)
+        return sp_elem_dict
+
+    def cmp_sp_elem(self, query_dict, doc_dict):
+        doc_score = []
+        for item in self.numelemdict.keys():
+            score = float(self.numelemdict[item])
+            if query_dict[item] == doc_dict[item]:
+                doc_score.append(score)
+            elif item == 'injure_count':
+                if query_dict[item][0] == 0:
+                    cur_score = score
+                    cur_score -= doc_dict[item][0] * 5
+                    cur_score -= abs(query_dict[item][1] - doc_dict[item][1]) * 2
+                    doc_score.append(cur_score)
+                elif query_dict[item][1] == 0:
+                    cur_score = score
+                    cur_score -= doc_dict[item][1] * 5
+                    cur_score -= abs(query_dict[item][0] - doc_dict[item][0]) * 2
+                    doc_score.append(cur_score)
+                else:
+                    cur_score = score
+                    cur_score -= abs(query_dict[item][0] - doc_dict[item][0]) * 2
+                    cur_score -= abs(query_dict[item][1] - doc_dict[item][1]) * 2
+                    doc_score.append(cur_score)
+            else:
+                if query_dict[item] == 0:
+                    doc_score.append(0)
+                else:
+                    cur_score = score / (1.0 + abs(query_dict[item] - doc_dict[item]) / query_dict[item])
+                    doc_score.append(cur_score)
+        return doc_score
+
+
+    def analysis_com_elem(self, text):
+        # 有无类要素，元素即分值
+        vec1 = np.zeros((len(self.elemdict)), dtype=float)
+        i = 0
+        for item in self.elemdict.keys():
+            pt_obj = re.compile(item)
+            se = pt_obj.search(text)
+            if se:
+                vec1[i] = self.elemdict[item]
+            i += 1
+        return vec1
+
 
     def analysis_query_text(self, file_name, crime_index):
         f = open(path_define.CRIMINAL_DIR + file_name)
@@ -50,14 +133,37 @@ class elem_analysis:
 
 class elem_rank:
     def search_as_dict(self, query_filename, crime_index, cmp_file_list):
-        ana_inst = elem_analysis()
-        query_vec = ana_inst.analysis_query_text(query_filename, crime_index)
-        i = 0
+        ana_inst = elem_analysis(crime_index)
+        f = open(path_define.CRIMINAL_DIR + query_filename)
+        text = f.read()
+        f.close()
+        text = text.decode('utf-8')
+        pattern = re.compile(u'书.记.员')
+        ps = pattern.search(text)
+        if ps:
+            text = text[:ps.start()]
+        print ','.join(ana_inst.elemdict.keys()).encode('utf-8')
+        print ','.join(ana_inst.numelemdict.keys()).encode('utf-8')
+        qsp_elem = ana_inst.analysis_sp_elem(text)
+        qvec = ana_inst.analysis_com_elem(text)
+        qvec = np.hstack((qvec, np.array(ana_inst.numelemdict.values())))
+        utils.debug_log(query_filename, qvec)
         rst_dict = {}
         for file_name in cmp_file_list:
-            rst_dict[file_name] = ana_inst.cmp_text_elem(file_name, query_vec, crime_index)
-            #print "file=", file_name, "score=", rst_dict[file_name]
-            i += 1
+            f = open(path_define.CRIMINAL_DIR + file_name)
+            text = f.read()
+            text = text.decode('utf-8')
+            pattern = re.compile(u'书.记.员')
+            ps = pattern.search(text)
+            if ps:
+                text = text[:ps.start()]
+            dsp_elem = ana_inst.analysis_sp_elem(text)
+            dsp_score = ana_inst.cmp_sp_elem(qsp_elem, dsp_elem)
+            dvec = ana_inst.analysis_com_elem(text)
+            dvec = np.hstack((dvec, dsp_score))
+            utils.debug_log(file_name, dvec)
+            rst_dict[file_name] = np.linalg.norm(qvec - dvec)
+            print 'score=', rst_dict[file_name]
         return rst_dict
 
 
