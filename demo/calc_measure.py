8e40241e8adfe7de5940f5a113379549e677961b
gwlin <gwlin@iflytek.com>
2017-03-20 15:51:07 +0800

    独立程序，相似案例统计刑期和罚金的均值和方差，基于已经得到[(case_name1 score1), (case_name2,score2),...] 的pkl数据, 统计结果打印到屏幕

--- demo/calc_measure.py
+++ demo/calc_measure.py
@@ -3,7 +3,8 @@
 
 import sys, os
 import numpy as np
-from common import const_data, metric, xls_parser
+from common import const_data, metric, xls_parser, utils, path_define
+import re
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -41,9 +42,55 @@ def get_my_rank(dir):
                                   metric.dcg_at_k(cur_rating, 1), metric.get_ndcg(cur_rating, 1), ]
     print rank_dict
 
+def statistic_para(name_list, para_name, dict_name2json):
+    '''
+    统计平均值和方差
+    :param name_list: 案件名列表
+    :param para_name: 待统计的参数名
+    :param dict_name2json: 案件名与其提取后的json
+    :return: 平均值和方差
+    '''
+    para_values = []
+    for name in name_list:
+        if name in dict_name2json:
+            value = dict_name2json[name][para_name]
+            if value != '':
+                value = int(value)
+            else:
+                value = 0
+            para_values.append(value)
+    print "values of %s are " % para_name
+    print para_values
+    para_values = np.asarray(para_values)
+    mean = np.mean(para_values)
+    var = np.var(para_values)
+    return mean, var
 
+def similar_case_diff_rst():
+    '''
+    相似案例统计刑期和罚金的均值和方差，基于已经得到[(case_name1 score1), (case_name2,score2),...] 的pkl数据, 统计结果打印到屏幕
+    :return:
+    '''
+    name2json_dict_pkl = path_define.MAP_NAME2JSON + '.pkl'
+    dict_name2json = utils.load_or_calc(name2json_dict_pkl, utils.gen_name2json_dict, \
+                                             path_define.MAP_NAME2JSON)
+    data_dir = "data/theft_2k_w0.1_0.1_0.4_0.4"
+    filelist = os.listdir(data_dir)
+    for file in filelist:
+        # "%s_gnr_cand_on%d_%s.pkl"
+        m = re.search("pkl$" , file)
+        if m:
+            data = utils.load_pyobj(os.path.join(data_dir, file))  # [(name,score), (name,score), ...]
+            m = re.match("[^.]+", file)
+            case_name = m.group(0)
+            name_list = list(zip(*data)[0])
+            mean, var = statistic_para(name_list[:100], 'sentenced_time', dict_name2json)
+            print "sentenced_time of %s (mean, var) are %f, %f" % ( case_name, mean, var )
+            mean, var = statistic_para(name_list[:100], 'money', dict_name2json)
+            print "money of %s (mean, var) are %f, %f" % ( case_name, mean, var )
 
 
 if __name__ == '__main__':
-    get_my_rank("../data/testcases_1221_0216/theft/")
+    #get_my_rank("../data/testcases_1221_0216/theft/")
+    similar_case_diff_rst()
 
